20a7da653d0baf8bd3b4a327129cada92463a4b4
Alyona Kolgasheva <kolgasheva065@gmail.com>
2023-12-11 03:33:29 +0200

    update build

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ProductsContainer from '../../components/ProductsContainer'
 import ItStarted from '../../components/ItStarted'
-import MainVideo from '../../components/MainVideo'
 import Advantages from '../../components/AdvantagesSection'
+import MainImage from "../../components/MainImage";
 
 export default function HomePage() {
   return (
     <>
-      <MainVideo />
+      <MainImage />
       <Advantages />
       <ProductsContainer />
       <ItStarted />
